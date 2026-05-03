5d179b4bfad15efe140e3457caa1d748aa2c081f
Kevin Antonio Antezana Florero <202401542@est.umss.edu>
2026-05-02 20:21:51 -0400

    feat(logic): extend class data with multiple entries and metadata

--- js/app.js
+++ js/app.js
@@ -10,3 +10,8 @@ const classes = [
   { id: 2, title: "Class 2", video: "https://www.youtube.com/embed/xyz456", topic: "Branches" },
   { id: 3, title: "Class 3", video: "https://www.youtube.com/embed/qwe789", topic: "Merge" }
 ];
+const classes = [
+  { id: 1, title: "Class 1", video: "https://www.youtube.com/embed/abc123", topic: "Intro" },
+  { id: 2, title: "Class 2", video: "https://www.youtube.com/embed/xyz456", topic: "Branches" },
+  { id: 3, title: "Class 3", video: "https://www.youtube.com/embed/qwe789", topic: "Merge" }
+];
